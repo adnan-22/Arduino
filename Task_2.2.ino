93e558d1fc98fbc28d1c7a187a0afe2c2dc268e1
Adnan Amin Siddiquee <38188511+adnan-22@users.noreply.github.com>
2018-12-26 15:16:11 +0600

    Add files via upload

---
+++ Task_2.2.ino
@@ -0,0 +1,11 @@
+float roomTemp;
+void setup() {
+  Serial.begin(9600);
+  analogReference(INTERNAL);//INTERNAK 1.1v for the Vref of ADC
+}
+
+void loop() {
+  roomTemp=(float)100*(1.1/1024)*analogRead(A0);//casting float
+  Serial.println(roomTemp,4);
+  delay(2000);
+}
